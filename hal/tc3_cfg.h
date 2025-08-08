745713031e24b2d1180f0b13fd2ce09f56a248d6
Brett Nicholas <7547222+bigbrett@users.noreply.github.com>
2025-08-08 13:48:55 -0600

    WIP: building wolfBoot on HSM, test-app failing

--- hal/tc3_cfg.h
+++ hal/tc3_cfg.h
@@ -3,7 +3,13 @@
 #ifndef TC3_CFG_H
 #define TC3_CFG_H
 
+
+#if defined(WOLFBOOT_AURIX_TC3XX_HSM)
+#define TC3_CFG_HAVE_ARM
+#else
 #define TC3_CFG_HAVE_TRICORE
+#endif
+
 #define TC3_CFG_HAVE_BOARD
 #define TC3_BOARD_TC375LITEKIT 1
 #define TC3_CFG_HAVE_WOLFBOOT
